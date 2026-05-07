e651cd0a481cf82b37939533f8f8855f38bb623e
SEHAN MADUSHANKA <sehanmadushanka0714@gmail.com>
2026-05-07 17:17:04 +0900

    Added users and Devices using Test file

--- device-service/src/test/java/com/smtech/device_service/DeviceServiceApplicationTests.java
+++ device-service/src/test/java/com/smtech/device_service/DeviceServiceApplicationTests.java
@@ -1,13 +1,42 @@
 package com.smtech.device_service;
 
+import com.smtech.device_service.entity.Device;
+import com.smtech.device_service.model.DeviceType;
+import com.smtech.device_service.repository.DeviceRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class DeviceServiceApplicationTests {
+	public static final int NUMBER_OF_DEVICES = 200;
+	public static final int USERS = 10;
+	@Autowired
+	private  DeviceRepository deviceRepository;
+
+
+
+    @Test
+	void contextLoads() {
+	}
 
 	@Test
-	void contextLoads() {
+	void addUsersToDB(){
+		for (int i = 1; i <= NUMBER_OF_DEVICES; i++) {
+			Device device = Device.builder()
+					.name("Device "+i)
+					.type(DeviceType.values()[i % DeviceType.values().length])
+					.location("Location "+((i % 3) +1))
+					.userId((long)((i % USERS)+1))
+					.build();
+			deviceRepository.save(device);
+
+
+		}
+
+		log.info("Device added successfully");
 	}
 
 }
--- user-service/src/test/java/com/smtech/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/smtech/user_service/UserServiceApplicationTests.java
@@ -1,13 +1,44 @@
 package com.smtech.user_service;
 
+import com.smtech.user_service.entity.User;
+import com.smtech.user_service.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class UserServiceApplicationTests {
 
-	@Test
+	public static final int NUMBER_OF_USERS = 10;
+
+	@Autowired
+	private  UserRepository userRepository;
+
+
+
+    @Test
 	void contextLoads() {
 	}
+
+	@Test
+	void addUsersToDB(){
+		for(int i = 1; i<= NUMBER_OF_USERS; i++){
+			User user= User.builder()
+					.name("User "+i)
+					.surname("Surname "+i)
+					.email("user"+i+"@example.com")
+					.address("Address "+i)
+					.alerting(i % 2 == 0)
+					.emergencyAlertingThreshold(1000.0+i)
+					.build();
+
+			userRepository.save(user);
+
+		}
+		log.info("Users added to database");
+	}
+
 
 }
